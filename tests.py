d6d13c42252a7f24036060637c7f1ac24c13fca8
ben <yipjones2@gmail.com>
2023-10-23 16:53:09 +0100

    test updates

--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
 import bentests as bt
 from board import *
+import colours
+from output import OutputManager
+
+output_manager = OutputManager()
 
 class Display(bt.testCase):
-	...
+	def testFirstBoardRow(self):
+		board = Board()
+		row = output_manager.getNthRowDisplay(board, 1)
+		bt.assertEquals(row,f"|{colours.GREY}|") #TODO: finish this.
 
 class PieceMethods(bt.testCase):
 	...
 
 class BoardCreation(bt.testCase):
 	def testBoardSizeIsString(self):
-		with bt.assertRaises(ValueError): # or even a custom exception
+		with bt.assertRaises(ValueError):
 			board = Board(size="a") #type: ignore Cuz it's wrong on purpose
 
 bt.test_all(
