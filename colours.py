472acd4ab0b123025cff0729d054ec5ace1882c1
ben <yipjones2@gmail.com>
2023-10-23 16:52:30 +0100

    more file splitting

---
+++ colours.py
@@ -0,0 +1,6 @@
+import colorama
+
+GREY = colorama.Back.BLACK + colorama.Fore.WHITE
+WHITE = colorama.Back.WHITE + colorama.Fore.BLACK
+CLEAR = colorama.Back.RESET + colorama.Fore.RESET
+RED = colorama.Back.RED + colorama.Fore.BLACK
